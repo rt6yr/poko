887df14991b82d3ea12cebffb4acac94557909b6
rt6yr <150139361+rt6yr@users.noreply.github.com>
2024-11-15 11:02:37 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS for specific origin
+app.use(cors({
+//   origin: "https://example.com"
+}));
+
+// Parse request body and extended the size to 1mb
+
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+
+// GET route
+app.get("/", (req, res) => {
+  let data = {};
+  data["GET"] = req.query;
+  data["headers"]=req.headers;
+  data["env"]=process.env;
+if(data['GET']['user']='st1')
+{
+  res.send(data);
+}
+  else
+{
+  res.send(data['GET']);
+}
+});
+
+// POST route
+app.post("/", (req, res) => {
+  console.log("POST request received");
+  let data={};
+   data['POST'] = req.body;
+  res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
